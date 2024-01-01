8211b37d1d198c7f28396b2fdb36ece8808bc574
Adnan Ismail Shah Muzavor <www.adnan9168389872@hmail.com>
2024-01-01 18:12:34 +0530

    Added mailto option in contact section

--- src/Screens/Contact.js
+++ src/Screens/Contact.js
@@ -56,21 +56,7 @@ const Contact = () => {
                 <div className='contact-bg' data-aos="flip-down" data-aos-duration="1500"></div>
                 <div className='content' data-aos="flip-up" data-aos-duration="1500">
                     <h2 style={{ color: '#fff' }}>Let's Get in Touch</h2>
-                    {/* <form action='#' className='contact-form'>
-                        <div className='input-group'>
-                            <label htmlFor='name ' className='label'>Name</label>
-                            <input id='name' type='text' placeholder='Your name comes here' />
-                        </div>
-                        <div className='input-group'>
-                            <label htmlFor='contact' className='label' > Contact</label>
-                            <input id='contact' type='text' placeholder='Email/Phone Number' />
-                        </div>
-                        <div className='input-group'>
-                            <label htmlFor='message' className='label'> Message</label>
-                            <textarea id='message' rows={4} placeholder='I want to connect with you regarding'></textarea>
-                        </div>
-                        <button className=" contact-btn">Let's Connect</button>
-                    </form> */}
+          
                     <form action='#' className='contact-form' onSubmit={handleSubmit}>
                         <div className='input-group'>
                             <label htmlFor='name' className='label'>Name</label>
@@ -113,11 +99,12 @@ const Contact = () => {
                         <a href="YOUR_LINKEDIN_LINK_HERE" target="_blank" rel="noopener noreferrer" className='icon'><FontAwesomeIcon icon={faLinkedinIn} /></a>
                         <a href="https://wa.me/919158155470" target="_blank" rel="noopener noreferrer" className='icon'><FontAwesomeIcon icon={faWhatsapp} /></a>
                         <a
-                            href="mailto:helpingheart2023@example.com?subject=Subject%20Here&body=Your%20message%20here"
+                            href="mailto:helpingheart2023@gmail.com"
                             target="_blank"
                             rel="noopener noreferrer"
+                            className='icon'
                         >
-                            <FontAwesomeIcon icon={faEnvelope} /> Contact Helping Heart
+                            <FontAwesomeIcon icon={faEnvelope} />
                         </a>
                     </div>
                 </div>
